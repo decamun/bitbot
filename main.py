4c0fb5fd6e6ad324b18ac9eff6015aba939f82a8
Devin Caplow-Munro <dcap@seas.upenn.edu>
2017-10-10 17:52:45 -0400

    bootstrap api functionality

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from bittrex import Bittrex
+import json
 
-my_bittrex
+def api_call(fn):
+    resp = fn()
+    assert resp['success']
+    return resp['result']
+
+
+with open('api_key.json','r') as f:
+    api_key = json.load(f)
+
+bittrex_api = Bittrex(api_key['key'], api_key['secret'])
+
+markets = api_call(bittrex_api.get_markets)
