4d29edacae57ab78e69a4662297162a447254830
Devin Caplow-Munro <decamun@gmail.com>
2017-12-30 13:09:31 -0800

    init

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bittrex import Bittrex
+from binance.client import Client
 import json
 import requests
 
@@ -7,7 +8,8 @@ def api_call(fn):
     assert resp['success']
     return resp['result']
 
-def scrape(filename='market_histories.json', interval='hour'):
+def scrape_bittrex(filename='market_histories_bittrex.json', interval='hour'):
+    # TODO convert to just scrape btc/eth/etc/xrp
     #check the markets
     markets = api_call(bittrex_api.get_markets)
 
@@ -25,9 +27,35 @@ def scrape(filename='market_histories.json', interval='hour'):
         json.dump(market_histories, f, indent=1)
 
 
-#open connection to bittrex
+def scrape_binance(filename='market_histories_binance.json', interval='hour'):
+    #TODO convert to just scrape btc/eth/etc/xrp and used binance api
+    #check the markets
+    markets = api_call(bittrex_api.get_markets)
+
+    #scrape market histories for all markets
+    market_histories = dict()
+    for market in markets:
+        MarketName = market["MarketName"]
+        print(MarketName)
+        url = "https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName=%s&tickInterval=%s"%(MarketName, interval)
+        market_histories[MarketName] = api_call(requests.get(url).json)
+        print('...done')
+
+    print("saving data to %s"%filename)
+    with open(filename, 'w') as f:
+        json.dump(market_histories, f, indent=1)
+
+#load api keys
 with open('api_key.json','r') as f:
     api_key = json.load(f)
-bittrex_api = Bittrex(api_key['key'], api_key['secret'])
+
+#open a connection to binance
+binance_client = Client(api_key['binance_key'], api_key['binance_secret'])
+
+
+#open connection to bittrex
+bittrex_api = Bittrex(api_key['bittrex_key'], api_key['bittrex_secret'])
 
 #things to do...
+prices = binance_client.get_all_tickers()
+print(prices)
